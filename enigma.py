5cd4929373d9fc08e79f1d7fb1cd7301e25f0f27
Your Name <javierfiestasbotella@gmail.com>
2021-02-15 00:35:56 +0100

    interfaz primitiva con tkinter

--- enigma.py
+++ enigma.py
@@ -1,5 +1,10 @@
 import random
 from colorama import init, Fore, Back, Style
+import sys
+from tkinter import *
+import tkinter
+from tkinter import messagebox
+
 class Reflector(): 
     
     def __init__(self):
@@ -94,8 +99,87 @@ def start():
         fin=r.conexion_decodifica(indice,e.rotor1)
         mensaje_trans=e.abecedario[fin]
         print(Fore.GREEN+mensaje_trans+Fore.WHITE,end="")
+def initial_position():
+    cambio_rotores=posicion_inicial.get()
+    for s in cambio_rotores:
+        posini.append(s)
+    a=posini[0]
+    b=posini[1]
+    c=posini[2]
+    #print("Sobre  la posicion: "+Fore.RED+a,b,c+" ",end="")
+    e.posicion_inicial(r.rotor1,r.rotor2,r.rotor3,a,b,c)
 
-start()
+
+
+
+def start2(mensaje):
+    global s
+    t=[]
+    for i in mensaje:#poner una condicion si lo escrito no esta en mayusculas ponerlas. poner tambien espacios para frases
+        indice=e.abecedario.index(i) 
+        indice=r.conexion(indice,e.rotor1)
+        indice=r.conexion(indice,e.rotor2)    
+        indice=r.conexion(indice,e.rotor3)
+        indice=rf.refleja(indice)
+        indice=r.conexion_decodifica(indice,e.rotor3)   
+        indice=r.conexion_decodifica(indice,e.rotor2)
+        fin=r.conexion_decodifica(indice,e.rotor1)
+        mensaje_trans=e.abecedario[fin]
+        #print(Fore.GREEN+mensaje_trans+Fore.WHITE,end="")
+        t.append(mensaje_trans)
+        resultado_texto["text"]=t
+        
+            
+def mensaje():
+    global mensaje_trans
+    texto_mensaje=etiqueta1.get()
+    start2(texto_mensaje)
+
+
+
+global etiqueta
+global etiqueta1
+global posicion_inicial
+global pantalla0
+
+pantalla0=Tk()
+pantalla0.geometry("800x600")
+pantalla0.title(" MAQUINA ENIGMA")
+pantalla0.iconbitmap("enigmaico.ico")
+pantalla0.config(bg="#2874A6" )
+    
+   
+titulo=Label(pantalla0,text="Máquina Enigma",bg="black",fg="white",width="300",height="2",font=('Terminal',25))
+titulo.pack()
+    
+Label(text="").pack()
+    
+
+posicion_inicial=Entry(pantalla0,font="helvetica 20", borderwidth=7,width=4)
+posicion_inicial.pack()
+
+Label(text="").pack()
+
+boton1=Button(pantalla0,text="Activa posición inicial",bg="gold4", borderwidth=7,font=('Consolas',10),command=initial_position)
+boton1.pack()
+Label(text="").pack()
+Label(text="Introduzca el mensaje:").pack()
+etiqueta1=Entry(pantalla0,font="helvetica 20", borderwidth=7,width=50,justify="center")
+etiqueta1.pack()
+Label(text="").pack()
+
+boton2=Button(pantalla0,text="Codifica y descodifica",bg="gold4", borderwidth=7,font=("Consolas",10),command=mensaje).pack()
+Label(text="").pack()
+Label(text="TRASCRIPCION:").pack()
+Label(text="").pack()
+    
+resultado_texto=Label(pantalla0)
+resultado_texto.pack()
+pantalla0.mainloop()
+
+
+
+
 
 
 
